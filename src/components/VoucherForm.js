7e0b7eea017bc3259e7ed75889886a9d0dcf4fd9
Tien <tienlehung1080@gmail.com>
2024-01-03 14:48:44 +0700

    first commit

---
+++ src/components/VoucherForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./../style.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const VoucherForm = () => {
+  const [voucher, setVoucher] = useState("");
+
+  const handleVoucherChange = (e) => {
+    setVoucher(e.target.value);
+  };
+
+  const handleApplyVoucher = () => {
+    if (voucher === "sheryl") {
+      toast.success("Tèn ten! Chúc bạn xài voucher vui vẻ ^^", {
+        position: "top-right",
+      });
+    } else {
+      toast.error("Voucher không đúng, vui lòng kiểm tra lại!", {
+        position: "top-right",
+      });
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleApplyVoucher();
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-header">Nhập voucher</h2>
+
+      <input
+        className="input-field"
+        type="text"
+        placeholder="Nhập voucher..."
+        value={voucher}
+        onChange={handleVoucherChange}
+        onKeyPress={handleKeyPress}
+      />
+
+      <button className="apply-button" onClick={handleApplyVoucher}>
+        Áp dụng voucher
+      </button>
+    </div>
+  );
+};
+
+export default VoucherForm;
